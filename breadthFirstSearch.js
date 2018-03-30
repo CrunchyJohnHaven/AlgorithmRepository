225cdcb2a76aa8a80105eb7bf5f819cd56ea0b2b
John Bradley <johnhavenbradley@gmail.com>
2018-03-29 19:05:18 -0700

    update

--- breadthFirstSearch.js
+++ breadthFirstSearch.js
@@ -9,4 +9,38 @@ var adjList = {
     "H": ["G", "I"],
     "I": ["D", "H"]
 }
-console.log("adjList: ", adjList)
+var origin = "A"
+var destination = "D"
+function breadthFirstSearch(origin, destination, adjList) {
+    var queue = [];
+    queue.push(origin)
+    var neighbors = []
+    var stack = []
+    var tracker = {}
+    tracker[origin] = [0, null]
+    while(!(destination in tracker) && (queue.length != 0)){
+        var hotseat = queue.shift();
+        for(a = 0; a < adjList[hotseat].length; a++) {
+            neighbors.push(adjList[hotseat][a])
+        }
+        for (var i = 0; i < neighbors.length; i++) {
+            if(!(neighbors[i] in tracker)) {
+                tracker[neighbors[i]] = [tracker[hotseat][0]+1, hotseat]
+                queue.push(neighbors[i])
+                if(neighbors[i] == destination) {
+                    break
+                }
+            }
+
+        }
+    }
+    if(destination in tracker){
+        return(["true", tracker])
+        }
+    else {
+        return([false, tracker])
+        }
+}
+
+var breadth = breadthFirstSearch(origin, destination, adjList)
+console.log(breadth)
